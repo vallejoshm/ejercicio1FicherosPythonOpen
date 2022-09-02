df45fa1f62469cc94a5bb485ba0451e7f809010a
Hernan Vallejos <vallejoshm@gmail.com>
2022-09-02 18:48:47 -0300

    segundo commit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 
 
-f = open('miFichero.txt', 'a')
+f = open('miFichero.txt', 'w')
 
 f.write('Este es mi primer fichero en python\n')
+
+f.close()
+
+f = open('miFichero.txt', 'a')
+
+f.write('Este es mi segundo intento\n')
 
 f.close()
